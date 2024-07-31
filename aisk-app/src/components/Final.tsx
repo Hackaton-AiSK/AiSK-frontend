95906c172c85e601a9640c082264054ebf7d0cc0
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-07-31 16:41:42 +0900

    feat: finished state screen

--- aisk-app/src/components/Final.tsx
+++ aisk-app/src/components/Final.tsx
@@ -9,6 +9,7 @@ const Final: React.FC<FinalProps> = ({ title }) => {
   return (
     <div className="final-container">
       <p className="final-title">주문 완료</p>
+      <p className="final-number">15번</p>
     </div>
   );
 };
